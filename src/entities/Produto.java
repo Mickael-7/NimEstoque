3deb9389761b6b53a5a31780d91e708e34064b8d
Mickael-7 <mickael.albuquerque@upe.br>
2024-10-14 19:56:14 -0300

    fix(Produto): retirar logica de notificação de observadores

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,10 +1,5 @@
 package entities;
 
-import observer.ObservadorEstoque;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Produto {
     private String nome;
     private int quantidade;
@@ -18,60 +13,19 @@ public class Produto {
         this.preco = preco;
     }
 
-    private List<ObservadorEstoque> observadores = new ArrayList<>();
-
-    public Produto(String nome, int quantidade,double preco) {
+    public Produto(String nome, int quantidade, double preco) {
         this.nome = nome;
         this.quantidade = quantidade;
         this.preco = preco;
     }
 
-    public void adicionarObservador(ObservadorEstoque observador) {
-        observadores.add(observador);
-    }
-
-    public void removerObservador(ObservadorEstoque observador) {
-        observadores.remove(observador);
-    }
-    private void notificarObservadores() {
-        if (this.quantidade <= 5) {
-            for (ObservadorEstoque observador : observadores) {
-                observador.atualizar(this, "Estoque crítico");
-            }
-        } else {
-            for (ObservadorEstoque observador : observadores) {
-                observador.atualizar(this, "Estoque atualizado");
-            }
-        }
-    }
-
     public void reduzirEstoque(int quantidade) {
         if (this.quantidade >= quantidade) {
             this.quantidade -= quantidade;
-            notificarObservadores();
         } else {
             System.out.println("Quantidade insuficiente em estoque.");
         }
     }
-
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public List<ObservadorEstoque> getObservadores() {
-        return observadores;
-    }
-
-    public void setObservadores(List<ObservadorEstoque> observadores) {
-        this.observadores = observadores;
-    }
-
-
 
     public String getNome() {
         return nome;
@@ -81,6 +35,7 @@ public class Produto {
         return quantidade;
     }
 
-
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
-
