a6fedc5ff38878e52030194687a5200ec202da53
Mickael-7 <mickael.albuquerque@upe.br>
2024-10-14 19:57:43 -0300

    fix(LogEstoque): modificar marcações de tempo

--- src/observer/LogEstoque.java
+++ src/observer/LogEstoque.java
@@ -1,16 +1,21 @@
 package observer;
 
 import entities.Produto;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class LogEstoque implements ObservadorEstoque {
     @Override
     public void atualizar(Produto produto, String mensagem) {
         try (FileWriter writer = new FileWriter("log_estoque.txt", true)) {
-            writer.write("Produto: " + produto.getNome() + " - " + mensagem + "\n");
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            String timestamp = dtf.format(LocalDateTime.now());
+            writer.write("[" + timestamp + "] Produto: " + produto.getNome() + " - " + mensagem + "\n");
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Erro ao registrar no log: " + e.getMessage());
         }
     }
 }
