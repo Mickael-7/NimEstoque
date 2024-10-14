50c9ed5e304daa4e1d8c68468d5f60c828caaab7
Mickael-7 <mickael.albuquerque@upe.br>
2024-10-14 19:55:51 -0300

    refactor(PedidoApp): Colocar usabilidades no terminal

--- src/PedidoApp.java
+++ src/PedidoApp.java
@@ -3,134 +3,137 @@ import entities.Produto;
 import factory.PedidoExpressoFactory;
 import factory.PedidoFactory;
 import factory.PedidoNormalFactory;
+import observer.GerenciadorEstoque;
+import observer.LogEstoque;
 import observer.RelatorioEstoque;
 import singleton.Estoque;
 import singleton.PedidoRepository;
 
-import javax.swing.*;
 import java.util.List;
+import java.util.Scanner;
 
 public class PedidoApp {
+
+    private static GerenciadorEstoque gerenciadorEstoque = new GerenciadorEstoque();
+
     public static void main(String[] args) {
-        // Inicializar estoque com singleton
+        Scanner scanner = new Scanner(System.in);
+
         Estoque estoque = Estoque.getInstancia();
         RelatorioEstoque re = new RelatorioEstoque();
+        LogEstoque le = new LogEstoque();
 
-        String[] opcoes = {"Cadastrar Produto", "Criar Pedido","Visualizar Pedidos", "Visualizar Estoque", "Gerar Relatorio", "Sair"};
+        gerenciadorEstoque.adicionarObservador(re);
+        gerenciadorEstoque.adicionarObservador(le);
+
+        String[] opcoes = {"Cadastrar Produto", "Criar Pedido", "Visualizar Pedidos", "Visualizar Estoque", "Gerar Relatório", "Sair"};
         int escolha;
 
         do {
-            // Menu de opções
-            escolha = JOptionPane.showOptionDialog(
-                    null, "Escolha uma opção", "Sistema de Pedidos",
-                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]
-            );
-
-            // Verificar se a janela foi fechada
-            if (escolha == JOptionPane.CLOSED_OPTION) {
-                escolha = 5; // Definir escolha como "Sair"
+            System.out.println("Escolha uma opção:");
+            for (int i = 0; i < opcoes.length; i++) {
+                System.out.println(i + ". " + opcoes[i]);
             }
 
+            escolha = scanner.nextInt();
+            scanner.nextLine();
+
             switch (escolha) {
-                case 0: // Cadastrar Produto
-                    cadastrarProduto();
+                case 0:
+                    cadastrarProduto(scanner);
                     break;
-                case 1: // Criar Pedido
-                    criarPedido();
+                case 1:
+                    criarPedido(scanner);
                     break;
                 case 2:
                     listarPedidos();
                     break;
-                case 3:// Visualizar Estoque
+                case 3:
                     visualizarEstoque();
                     break;
                 case 4:
                     re.gerarRelatorio(estoque.getProdutos());
                     break;
-                case 5:// Sair
-                    JOptionPane.showMessageDialog(null, "Saindo do sistema.");
+                case 5:
+                    System.out.println("Saindo do sistema.");
                     break;
                 default:
+                    System.out.println("Opção inválida. Tente novamente.");
                     break;
             }
         } while (escolha != 5);
+
+        scanner.close();
     }
 
-    public static void cadastrarProduto() {
-        // Pergunta o nome do produto
-        String nomeProduto = JOptionPane.showInputDialog("Informe o nome do produto:");
+    public static void cadastrarProduto(Scanner scanner) {
+        System.out.print("Informe o nome do produto: ");
+        String nomeProduto = scanner.nextLine();
 
-        String precoProdutoStr = JOptionPane.showInputDialog("Informe a preço do produto:");
-        int precoProduto = Integer.parseInt(precoProdutoStr);
+        System.out.print("Informe o preço do produto: ");
+        int precoProduto = Integer.parseInt(scanner.nextLine());
 
-        // Pergunta a quantidade do produto
-        String quantidadeProdutoStr = JOptionPane.showInputDialog("Informe a quantidade do produto:");
-        int quantidadeProduto = Integer.parseInt(quantidadeProdutoStr);
+        System.out.print("Informe a quantidade do produto: ");
+        int quantidadeProduto = Integer.parseInt(scanner.nextLine());
 
-        // Criar e adicionar produto ao estoque
-        Produto novoProduto = new Produto(nomeProduto, quantidadeProduto,precoProduto);
+        Produto novoProduto = new Produto(nomeProduto, quantidadeProduto, precoProduto);
         Estoque.getInstancia().adicionarProduto(novoProduto);
 
-        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+        gerenciadorEstoque.atualizarProduto(novoProduto, quantidadeProduto);
+
+        System.out.println("Produto cadastrado com sucesso!");
     }
 
-    public static void criarPedido() {
-        // Perguntar o tipo de pedido
-        String[] tiposPedido = {"Normal", "Expresso"};
-        String tipoPedido = (String) JOptionPane.showInputDialog(
-                null, "Selecione o tipo de pedido:", "Tipo de Pedido",
-                JOptionPane.QUESTION_MESSAGE, null, tiposPedido, tiposPedido[0]
-        );
+    public static void criarPedido(Scanner scanner) {
+        System.out.println("Selecione o tipo de pedido:");
+        System.out.println("1. Normal");
+        System.out.println("2. Expresso");
+        int tipoPedidoEscolhido = Integer.parseInt(scanner.nextLine());
 
-        String quantidadePedidoStr = JOptionPane.showInputDialog("Informe a quantidade do produto:");
-        int quantidadePedido = Integer.parseInt(quantidadePedidoStr);
+        String tipoPedido = tipoPedidoEscolhido == 1 ? "Normal" : "Expresso";
 
-        // Selecionar a fábrica de pedido correta
-        PedidoFactory pedidoFactory = null;
-        if (tipoPedido.equals("Normal")) {
-            pedidoFactory = new PedidoNormalFactory();
-        } else if (tipoPedido.equals("Expresso")) {
-            pedidoFactory = new PedidoExpressoFactory();
-        }
+        System.out.print("Informe a quantidade do produto: ");
+        int quantidadePedido = Integer.parseInt(scanner.nextLine());
 
-        // Perguntar qual produto foi comprado
+        PedidoFactory pedidoFactory = tipoPedido.equals("Normal") ? new PedidoNormalFactory() : new PedidoExpressoFactory();
+
         Estoque estoque = Estoque.getInstancia();
         String[] produtos = estoque.getProdutos().stream().map(Produto::getNome).toArray(String[]::new);
-        String produtoEscolhido = (String) JOptionPane.showInputDialog(
-                null, "Selecione o produto:", "Produto",
-                JOptionPane.QUESTION_MESSAGE, null, produtos, produtos[0]
-        );
+        System.out.println("Selecione o produto:");
+        for (int i = 0; i < produtos.length; i++) {
+            System.out.println((i + 1) + ". " + produtos[i]);
+        }
+        int produtoEscolhidoIndex = Integer.parseInt(scanner.nextLine()) - 1;
+        String produtoEscolhido = produtos[produtoEscolhidoIndex];
 
-        // Atualizar o estoque
-        Pedido pedido = pedidoFactory.criarPedido(estoque.buscarProduto(produtoEscolhido).getPreco() * quantidadePedido,quantidadePedido,estoque.buscarProduto(produtoEscolhido));
+        Produto produto = estoque.buscarProduto(produtoEscolhido);
+
+        Pedido pedido = pedidoFactory.criarPedido(produto.getPreco() * quantidadePedido, quantidadePedido, produto);
         pedido.processar();
 
         PedidoRepository.getInstancia().adicionarPedido(pedido);
-        // Criar e processar o pedido
-        Produto produto = estoque.buscarProduto(produtoEscolhido);
+
         produto.reduzirEstoque(quantidadePedido);
+        gerenciadorEstoque.atualizarProduto(produto, produto.getQuantidade());
     }
 
     public static void listarPedidos() {
         List<Pedido> pedidos = PedidoRepository.getInstancia().listarPedidos();
         if (pedidos.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Nenhum pedido foi feito até o momento.");
+            System.out.println("Nenhum pedido foi feito até o momento.");
         } else {
-            StringBuilder sb = new StringBuilder();
+            System.out.println("Lista de Pedidos:");
             for (Pedido pedido : pedidos) {
-                sb.append(pedido.getClass().getSimpleName()).append(" - ").append(pedido.getProduto().getNome())
-                        .append(" - R$ ").append(pedido.getValor()).append("\n");
+                System.out.println(pedido.getClass().getSimpleName() + " - " + pedido.getProduto().getNome() + " - R$ " + pedido.getValor());
             }
-            JOptionPane.showMessageDialog(null, sb.toString(), "Lista de Pedidos", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
     public static void visualizarEstoque() {
         Estoque estoque = Estoque.getInstancia();
-        StringBuilder sb = new StringBuilder();
+        System.out.println("Estoque Atual:");
         for (Produto produto : estoque.getProdutos()) {
-            sb.append(produto.getNome()).append(" - Quantidade: ").append(produto.getQuantidade()).append("\n").append(" - Preço Unitário: ").append(produto.getPreco()).append("\n");
+            System.out.println(produto.getNome() + " - Quantidade: " + produto.getQuantidade() + " - Preço Unitário: R$ " + produto.getPreco());
         }
-        JOptionPane.showMessageDialog(null, sb.toString(), "Estoque Atual", JOptionPane.INFORMATION_MESSAGE);
     }
 }
